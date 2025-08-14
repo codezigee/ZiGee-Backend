a05b68656ccd46ec13fa9a84eb5a7099d8e3797a
hye-inA <hye.ina0130@gmail.com>
2025-08-14 23:13:34 +0900

    프로젝트 생성

---
+++ src/main/java/academy/zigee/ZigeeApplication.java
@@ -0,0 +1,13 @@
+package academy.zigee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZigeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZigeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/academy/zigee/ZigeeApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.zigee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZigeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
