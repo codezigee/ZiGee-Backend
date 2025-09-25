a8ead40549dcec6ad9626563fec07ddfcb533001
hye-inA <hye.ina0130@gmail.com>
2025-09-25 17:22:43 +0900

    feat : 사용자 조회 서비스 초기 세팅

--- src/main/java/academy/zigee/controller/MemberController.java
+++ src/main/java/academy/zigee/controller/MemberController.java
@@ -1,4 +1,33 @@
 package academy.zigee.controller;
 
+import academy.zigee.domain.Member;
+import academy.zigee.repository.MemberDaoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class MemberController {
+
+    private MemberDaoService service ;
+
+
+    // MemberController 생성자 역시 Spring Context가 호출 <- 생성함에 있어서 그 전에 주입된 MemberDaoService 인스턴스 값을 가지고 오겠다
+    public MemberController(MemberDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/members")
+    public List<Member> retrieveAllMembers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/members/{id}")
+    public Member retrieveMember(@PathVariable int id){
+        return service.findOne(id);
+    }
 }
